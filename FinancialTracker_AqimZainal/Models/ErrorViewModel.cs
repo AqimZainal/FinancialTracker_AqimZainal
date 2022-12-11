4b183b54a66ae62b39b3adc913a7077e4632181e
AqimZainal <aqim.zainal@gmail.com>
2022-12-11 11:33:07 -0600

    Add project files.

---
+++ FinancialTracker_AqimZainal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinancialTracker_AqimZainal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
